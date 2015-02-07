b13ad58ddaa6e8905820cdc401c90937408d2a37
David Maurer <davemaurermail@gmail.com>
2015-02-06 22:47:21 -0700

    initial

--- exercises/all_pattern_test.rb
+++ exercises/all_pattern_test.rb
@@ -17,6 +17,7 @@ class AllPatternTest < Minitest::Test
     numbers = [0, 0, 0, 0, 1, 0, 0, 0]
     all_zeros = true
     numbers.each do |number|
+      all_zeros = false unless number.zero
       # Your code goes here
     end
     refute all_zeros
